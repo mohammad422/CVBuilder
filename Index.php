e437ee43ace1d406186a463c8c9ad34989fd0498
mohammad422 <mohammadjavad.darestani@gmail.com>
2019-12-04 09:33:44 +0330

    first

---
+++ Index.php
@@ -0,0 +1,10 @@
+<?php
+require_once "Config/Config.php";
+session_start();
+
+if (isset($_SESSION["user_id"])){
+    header("location:Controllers/Dashboard.controller.php");
+}
+
+require_once "Views/Index.view.php";
+//first
